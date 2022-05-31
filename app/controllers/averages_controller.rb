0f02fcc5058c911aaa4f0b5952f6a0ebff43f311
Michael Brandt <msbrandt00@gmail.com>
2022-05-30 22:10:01 -0600

    Feature: index, change_pitch_type, show actions

--- app/controllers/averages_controller.rb
+++ app/controllers/averages_controller.rb
@@ -1,5 +1,23 @@
 class AveragesController < ApplicationController 
     def index 
-        @averages = Pitcher.averages('Sinker')
+        if params[:type].present? 
+            @type = params[:type]
+            if params[:name].present?
+                @averages = Pitcher.averages(params[:type]).search(params[:name])
+            else 
+                @averages = Pitcher.averages(params[:type])
+            end
+        else 
+            @averages = Pitcher.averages('Changeup')
+            @type = 'Changeup'
+        end  
+    end
+
+    def change_pitch_type 
+        redirect_to :action => :index, type: params[:type], name: params[:name]
+    end
+
+    def show 
+        @pitcher = Pitcher.find(params[:id])
     end
 end
